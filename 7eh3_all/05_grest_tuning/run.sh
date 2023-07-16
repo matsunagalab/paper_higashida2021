bfb5dfebf8ea76278c62a433409f27732a0c2085
ymatsunaga <ymatsunaga@mail.saitama-u.ac.jp>
2023-07-16 12:10:30 +0900

    7eh3 all genesis input files

---
+++ 7eh3_all/05_grest_tuning/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -p all
+#SBATCH -J 7eh3
+#SBATCH -n 32  #num of total mpi processes
+#SBATCH -c 1  #num of threads per mpi processes
+#SBATCH -o run.log
+
+# set the number of OpenMP threads
+export OMP_NUM_THREADS=1
+export CUDA_VISIBLE_DEVICES="2,3,4,5,6,7,8,9"
+
+# perform equilibration with SPDYN by using 8 MPI processes
+mpirun -np 32 /opt/genesis/bin/spdyn.cuda run.inp
+
